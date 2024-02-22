92992343978456f888a8d94ced4c8a530f7c90cd
Aaronkeener64 <edward64432@gmail.com>
2024-02-22 16:19:12 -0500

    insercion de datos

---
+++ archivo.js
@@ -0,0 +1,12 @@
+var nombre = " dianita" ;
+var edad = "21";
+var presenteoNo = true;
+var apellido = "Rojas";
+
+
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Presente: " + presenteoNo);
+console.log("Apellido: " + apellido);
+
+//alert("hola mundo con javascript" + nombre);
